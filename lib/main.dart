4891c7822a1f5834cd935fa773766bc5bc04bd11
Rod97139 <fayaflame@gmail.com>
2024-04-20 00:49:42 +0200

    cross platform ok with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_pizza_app/app.dart';
 import 'package:flutter_pizza_app/simple_bloc_observer.dart';
+import 'package:user_repository/user_repository.dart';
+import 'firebase_options.dart';
+
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+
+  );
   Bloc.observer = SimpleBlocObserver();
-  runApp(const MyApp());
+  runApp(MyApp(FireBaseUserRepo()));
 }
 
