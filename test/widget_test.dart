8d303373a3b295e70a7164b2ba0575f68f11c530
Rod97139 <fayaflame@gmail.com>
2024-04-19 22:50:24 +0200

    update test widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,11 +9,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_pizza_app/app.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_pizza_app/main.dart';
+import 'package:user_repository/user_repository.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp(FireBaseUserRepo()));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
