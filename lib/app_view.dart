2181bfdb8f4e8355545b05d709a38b7bde39e4dc
Rod97139 <fayaflame@gmail.com>
2024-04-20 01:12:32 +0200

    start adding views

--- lib/app_view.dart
+++ lib/app_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_pizza_app/blocs/authentication_bloc/authentication_bloc.dart';
+import 'package:flutter_pizza_app/screens/auth/views/welcome_screen.dart';
+import 'package:flutter_pizza_app/screens/home/views/home_screen.dart';
 
 class MyAppView extends StatelessWidget {
   const MyAppView({super.key});
@@ -7,6 +11,23 @@ class MyAppView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pizza delivery',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.light(
+          background: Colors.grey.shade100,
+          onBackground: Colors.black,
+          primary: Colors.blue,
+          onPrimary: Colors.white,
+        ),
+      ),
+      home: BlocBuilder<AuthenticationBloc, AuthenticationState>(
+        builder: (context, state) {
+          if (state.status == AuthenticationStatus.authenticated) {
+            return const HommeScreen();
+          }
+          return const WelcomeScreen();
+        },
+      ),
     );
   }
 }
